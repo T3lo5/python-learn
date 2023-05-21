fbf34336ae7d07afeb5816aacce739c0b895e3c5
Anderson Franciscato <andersonwsf@gmail.com>
2023-05-21 15:29:32 -0300

    desafio criado e finalizado

---
+++ testeback/lacrei/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Lacrei(models.Model):
+    id_
+
+
+
+
+
+
+
+# TODO: Cadastrar profissional, editar e deletar. Deve aceitar nome social.
+# Cadastrar consultas, editar e deletar - data e pessoa profissional vinculada
+# Pesquisar pelo id do profissional
